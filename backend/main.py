acae57693bb9632afba57985e135f9456805b18f
Jefferson Vega <99852874+jeffersonvegag@users.noreply.github.com>
2025-08-09 00:53:01 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -46,9 +46,13 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://app-tesis-akira-frontend.onrender.com",
+        "http://localhost:3000",
+        "http://localhost:5173"  # para desarrollo con Vite
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
